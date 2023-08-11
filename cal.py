f10b4a1be8f3ec7fc487af95a5656f2ada267c85
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-08-11 10:18:32 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- cal.py
+++ cal.py
@@ -49,7 +49,7 @@ def main_cal():
         print("7. Sin")
         print("8. Cos")
         print("9. Tan")
-        print("Press 0 to exit Form Calculator")
+        print("Press s to exit Form Calculator")
 
         print("")
         print("--------------------------------------------------------------------")
@@ -57,7 +57,7 @@ def main_cal():
 
         operator = input("Enter Number of Operation (1/2/3/4/5/6/7/8/9) : ")
 
-        if operator == '0':
+        if operator == 's':
             print("Exiting From Calculator...")
             break
 
