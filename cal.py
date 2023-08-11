5c1b3bc15b04cad77b1f828e9e61f60a7dca0c8a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-08-11 08:41:47 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- cal.py
+++ cal.py
@@ -31,6 +31,9 @@ def main_cal():
         elif operator in ('4'):
             num1 = float(input("Enter First Number : "))
             num2 = float(input("Eenter the number that want to Divide : "))
+
+        elif operator in ('5'):
+            num1 = float(input("Enter the Number that want to get Square Root : "))
     else:
         print("You Enterd Operation is Invalid Operation")
 
