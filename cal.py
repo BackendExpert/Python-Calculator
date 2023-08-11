2761b971e02e28cf777e3e6f6f9974fb9d29e0f4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-08-11 08:24:51 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- cal.py
+++ cal.py
@@ -3,3 +3,9 @@ import math
 def main_cal():
     print("Scientific Calculator")
     print("Operations")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Square Root")
+    print("6. Power")
